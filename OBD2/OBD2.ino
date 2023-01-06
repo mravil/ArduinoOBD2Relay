fb1d6b7f7cb82b264287cc9c364aebcf9639af7e
mravil <mravil@mravil.net>
2023-01-06 08:38:13 +0700

    Add files via upload

--- OBD2/OBD2.ino
+++ OBD2/OBD2.ino
@@ -4,13 +4,101 @@
 #define PID_COOLANT_TEMP 0x05
 #define PID_AMBIENT_TEMP 0x46
 
-
+// Подключаем библиотеки
 #include <SPI.h>
 #include <mcp_can.h>
 #include <SD.h>
+#include <TimerOne.h>
 
+// MCP2515
 #define CAN0_INT 2                                              // Set INT to pin 2  <--------- CHANGE if using different pin number
 MCP_CAN CAN0(10);                                               // Set CS to pin 10 <--------- CHANGE if using different pin number
+  
+// Считанную температуру будем хранить в массиве, сразу ининиализируем его на 80 градусов 
+int CoolantTempArray[9] = {80,80,80,80,80,80,80,80,80};
+int CoolantTemp;
+
+// Прототипы функций
+void CanInit(); //
+unsigned char * receivePID(unsigned char __pid); //
+int ReadTemp(); //
+
+//Первичная конфигурация платы и периферии 
+void setup()
+{
+  CanInit();
+  Timer1.initialize(500);
+  Timer1.attachInterrupt(ReadTemp);
+  Serial.begin(115200);
+  
+}
+
+void loop()
+{
+  unsigned char * getData;
+  getData = receivePID(PID_ENGINE_RPM);
+  uint16_t rpm = (256*(*(getData + 3)) + *(getData + 4))/4;
+  Serial.print("   RPM ");
+  Serial.println(rpm);
+  getData = receivePID(PID_AMBIENT_TEMP);
+  uint8_t atemp = *(getData + 3) - 40;
+  Serial.print("   Ambient  ");
+  Serial.println(atemp);
+  
+}
+
+
+//Functions
+//Чтение температуры
+int ReadTemp(){
+  for (char i=0;i<8;++i){
+    CoolantTempArray[i] = CoolantTempArray[i+1]; // Смещаем элементы массива на один порядок вправо
+  } //for
+
+  unsigned char * getData;
+  getData = receivePID(PID_COOLANT_TEMP);     // Получаем новое значение температуры ОЖ 
+  CoolantTempArray[8] = *(getData + 3) - 40;  // и заносим в конец массива
+
+  int TempArray[9]; //Создаем временный массив из элементов массива CoolantTempArray
+  for(char i=0;i<9;++i){
+    TempArray[i] = CoolantTempArray[i];
+  }
+  qsort(TempArray,10,sizeof(int),cmpfunc);    // Сортируем временный массив, в начале и в конце будут ошибочные данные, если конечно будут
+  CoolantTemp = TempArray[4];                 // в середине будет верное значение температуры
+  return CoolantTemp;
+} //readTemp
+
+// Вспомогательнся функция сравнения для функции qsort
+int cmpfunc (const void * a, const void * b) {
+   return ( *(int*)a - *(int*)b );
+} //cmpfunc
+
+void CanInit(){
+  if (CAN0.begin(MCP_STDEXT, CAN_500KBPS, MCP_8MHZ) == CAN_OK)  // Initialize MCP2515 running at 16MHz with a baudrate of 500kb/s and the masks and filters disabled.
+    { 
+      Serial.println("MCP2515 Initialized Successfully!");
+    }
+  else 
+    {
+      Serial.println("Error Initializing MCP2515...");
+      while (1);
+    }
+
+  //initialise mask and filter to allow only receipt of 0x7xx CAN IDs
+  CAN0.init_Mask(0, 0, 0x07000000);             // Init first mask...
+  CAN0.init_Mask(1, 0, 0x07000000);             // Init second mask...
+  
+  for (uint8_t i = 0; i < 6; ++i) {
+    CAN0.init_Filt(i, 0, 0x07000000);           // Init filters
+  }
+  
+  CAN0.setMode(MCP_NORMAL);                     // Set operation mode to normal so the MCP2515 sends acks to received data.
+
+  pinMode(CAN0_INT, INPUT);                     // Configuring pin for /INT input
+
+  Serial.println("Sending and Receiving OBD-II_PIDs");
+} //CanInit()
+
 
 unsigned char * receivePID(unsigned char __pid)
 {
@@ -49,56 +137,6 @@ unsigned char * receivePID(unsigned char __pid)
    
    return rxBuf;
 } //end receivePID
-
-void setup()
-{
-  Serial.begin(115200);
-
-  
-  if (CAN0.begin(MCP_STDEXT, CAN_500KBPS, MCP_8MHZ) == CAN_OK)  // Initialize MCP2515 running at 16MHz with a baudrate of 500kb/s and the masks and filters disabled.
-    { 
-      Serial.println("MCP2515 Initialized Successfully!");
-    }
-  else 
-    {
-      Serial.println("Error Initializing MCP2515...");
-      while (1);
-    }
-
-  //initialise mask and filter to allow only receipt of 0x7xx CAN IDs
-  CAN0.init_Mask(0, 0, 0x07000000);             // Init first mask...
-  CAN0.init_Mask(1, 0, 0x07000000);             // Init second mask...
-  
-  for (uint8_t i = 0; i < 6; ++i) {
-    CAN0.init_Filt(i, 0, 0x07000000);           // Init filters
-  }
-  
-  CAN0.setMode(MCP_NORMAL);                     // Set operation mode to normal so the MCP2515 sends acks to received data.
-
-  pinMode(CAN0_INT, INPUT);                     // Configuring pin for /INT input
-
-  Serial.println("Sending and Receiving OBD-II_PIDs");
-}
-
-void loop()
-{
-  unsigned char * getData;
-  getData = receivePID(PID_COOLANT_TEMP);
-  uint8_t ctemp = *(getData + 3) - 40;
-  Serial.print("   T ");
-  Serial.println(ctemp);
-  getData = receivePID(PID_ENGINE_RPM);
-  uint16_t rpm = (256*(*(getData + 3)) + *(getData + 4))/4;
-  Serial.print("   RPM ");
-  Serial.println(rpm);
-  getData = receivePID(PID_AMBIENT_TEMP);
-  uint8_t atemp = *(getData + 3) - 40;
-  Serial.print("   Ambient  ");
-  Serial.println(atemp);
-  
-}
-
-
 
 
 void notUsed(char __pid)
